98263731db9b5c3ee7b7ef398ebc146da44308f8
Zach-0313 <malta.zachary@gmail.com>
2023-04-21 11:16:49 -0400

    Added Back Buttons to the popup menus

--- SWE 3313 Restaurant Automation/TableStatus/TableStatus.Designer.cs
+++ SWE 3313 Restaurant Automation/TableStatus/TableStatus.Designer.cs
@@ -28,51 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.New_Order = new System.Windows.Forms.Button();
-            this.Payment = new System.Windows.Forms.Button();
-            this.Table_Status = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            New_Order = new Button();
+            Payment = new Button();
+            Table_Status = new Button();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // New_Order
             // 
-            this.New_Order.Location = new System.Drawing.Point(142, 132);
-            this.New_Order.Name = "New_Order";
-            this.New_Order.Size = new System.Drawing.Size(89, 23);
-            this.New_Order.TabIndex = 0;
-            this.New_Order.Text = "New Order";
-            this.New_Order.UseVisualStyleBackColor = true;
-            this.New_Order.Click += new System.EventHandler(this.New_Order_Click);
+            New_Order.Location = new Point(142, 132);
+            New_Order.Name = "New_Order";
+            New_Order.Size = new Size(89, 23);
+            New_Order.TabIndex = 0;
+            New_Order.Text = "New Order";
+            New_Order.UseVisualStyleBackColor = true;
+            New_Order.Click += New_Order_Click;
             // 
             // Payment
             // 
-            this.Payment.Location = new System.Drawing.Point(142, 180);
-            this.Payment.Name = "Payment";
-            this.Payment.Size = new System.Drawing.Size(89, 23);
-            this.Payment.TabIndex = 1;
-            this.Payment.Text = "Payment";
-            this.Payment.UseVisualStyleBackColor = true;
+            Payment.Location = new Point(142, 180);
+            Payment.Name = "Payment";
+            Payment.Size = new Size(89, 23);
+            Payment.TabIndex = 1;
+            Payment.Text = "Payment";
+            Payment.UseVisualStyleBackColor = true;
             // 
             // Table_Status
             // 
-            this.Table_Status.Location = new System.Drawing.Point(142, 229);
-            this.Table_Status.Name = "Table_Status";
-            this.Table_Status.Size = new System.Drawing.Size(89, 23);
-            this.Table_Status.TabIndex = 2;
-            this.Table_Status.Text = "Table Status";
-            this.Table_Status.UseVisualStyleBackColor = true;
+            Table_Status.Location = new Point(142, 229);
+            Table_Status.Name = "Table_Status";
+            Table_Status.Size = new Size(89, 23);
+            Table_Status.TabIndex = 2;
+            Table_Status.Text = "Table Status";
+            Table_Status.UseVisualStyleBackColor = true;
+            Table_Status.Click += Table_Status_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(256, 386);
+            button1.Name = "button1";
+            button1.Size = new Size(89, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += BackButton_Click;
             // 
             // TableStatus
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 450);
-            this.Controls.Add(this.Table_Status);
-            this.Controls.Add(this.Payment);
-            this.Controls.Add(this.New_Order);
-            this.Name = "TableStatus";
-            this.Text = "TableStatus";
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(379, 450);
+            Controls.Add(button1);
+            Controls.Add(Table_Status);
+            Controls.Add(Payment);
+            Controls.Add(New_Order);
+            Name = "TableStatus";
+            Text = "TableStatus";
+            ResumeLayout(false);
         }
 
         #endregion
@@ -80,5 +92,6 @@
         private System.Windows.Forms.Button New_Order;
         private System.Windows.Forms.Button Payment;
         private System.Windows.Forms.Button Table_Status;
+        private Button button1;
     }
 }
