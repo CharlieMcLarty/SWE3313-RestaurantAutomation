d86f423adf0eceb5b199723b4b3d22e4f439dd99
Zach-0313 <malta.zachary@gmail.com>
2023-04-22 16:16:32 -0400

    Added some class documentation. Added UML style diagrams

--- SWE 3313 Restaurant Automation/Users/Employee.cs
+++ SWE 3313 Restaurant Automation/Users/Employee.cs
@@ -1,30 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWE_3313_Restaurant_Automation.Users
+﻿namespace SWE_3313_Restaurant_Automation.Users
 {
     internal abstract class Employee
     {
+        /// <summary>
+        /// The ID number associated with this user
+        /// </summary>
         protected int employeeID;
+        /// <summary>
+        /// The saved username for this user
+        /// </summary>
         protected String? username;
+        /// <summary>
+        /// The saved password for this user
+        /// </summary>
         protected String? password;
 
-        
+
+        /// <summary>
+        /// Sets this user's username
+        /// </summary>
         public void SetUsername(String Username)
         {
             this.username = Username;
         }
+        /// <summary>
+        /// Sets this user's password
+        /// </summary>
         public void SetPassword(String Password)
         {
             this.password = Password;
         }
+        /// <summary>
+        /// Returns this user's stored password
+        /// </summary>
+        /// <returns></returns>
         public String GetPassword()
         {
             return password;
         }
+        /// <summary>
+        /// Returns this user's stored username
+        /// </summary>
         public String GetUsername()
         {
             return username;
